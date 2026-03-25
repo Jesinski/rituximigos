5d181b033e0d4d0bc2c5a4731a58b364e6277e22
Gustavo Jesinski <gustavojesinskib@gmail.com>
2026-03-25 20:16:47 -0300

    safer deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ STAMP=$(date +%s)
 # Stamp asset URLs in index.html
 sed -i '' -E "s/(style\.css|app\.js)\?v=[0-9]+/\1?v=$STAMP/g" index.html
 
-git add -A
+git add index.html
 git commit -m "deploy v$STAMP"
 git push origin HEAD
 
