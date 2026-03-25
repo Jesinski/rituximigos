13e8617fd8265354107dbe385bdfe014ff310ada
Gustavo Jesinski <gustavojesinskib@gmail.com>
2026-03-25 20:19:59 -0300

    fix local server

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const MIME = {
 };
 
 http.createServer((req, res) => {
-  let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
+  const urlPath = req.url.split("?")[0];
+  let filePath = path.join(__dirname, urlPath === "/" ? "index.html" : urlPath);
   const ext = path.extname(filePath);
 
   fs.readFile(filePath, (err, data) => {
